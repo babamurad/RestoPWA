3f6dc09b290afcf4c817aa30eb05528486586dfb
Babamurad <babamurad2010@yandex.ru>
2026-06-29 14:57:05 +0500

    fix: Remove double quotes from x-data comment to prevent HTML parser breakage

--- resources/views/filament/forms/components/delivery-zone-map.blade.php
+++ resources/views/filament/forms/components/delivery-zone-map.blade.php
@@ -18,7 +18,7 @@
     initMap() {
         ymaps.ready(() => {
             const mapContainer = this.$refs.map;
-            mapContainer.innerHTML = ''; // Очищаем надпись "Загрузка карты..."
+            mapContainer.innerHTML = ''; // Очищаем надпись Загрузка карты
             this.map = new ymaps.Map(mapContainer, {
                 center: [39.0886, 63.5593], // Туркменабат
                 zoom: 12,
