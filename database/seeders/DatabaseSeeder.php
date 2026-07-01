811989aaf27a67546b278a82d4ac627c24db8c8e
Babamurad <babamurad2010@yandex.ru>
2026-07-01 15:12:41 +0500

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,18 +20,22 @@ class DatabaseSeeder extends Seeder
             RestaurantSeeder::class,
         ]);
 
-        User::factory()->create([
-            'name' => 'Admin User',
-            'email' => 'admin@restopwa.local',
-            'password' => 'password',
-            'role' => UserRole::ADMIN,
-        ]);
+        if (!User::where('email', 'admin@restopwa.local')->exists()) {
+            User::factory()->create([
+                'name' => 'Admin User',
+                'email' => 'admin@restopwa.local',
+                'password' => 'password',
+                'role' => UserRole::ADMIN,
+            ]);
+        }
 
-        User::factory()->create([
-            'name' => 'Test Client',
-            'email' => 'test@example.com',
-            'password' => 'password',
-            'role' => UserRole::CLIENT,
-        ]);
+        if (!User::where('email', 'test@example.com')->exists()) {
+            User::factory()->create([
+                'name' => 'Test Client',
+                'email' => 'test@example.com',
+                'password' => 'password',
+                'role' => UserRole::CLIENT,
+            ]);
+        }
     }
 }
